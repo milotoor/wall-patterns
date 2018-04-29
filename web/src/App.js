03e5b3b8b100e786d7873ebc3f4e6675a1e7d80e
Milo Toor <mtoor@omicia.com>
2018-04-28 23:45:40 -0700

    Render with material-ui

--- web/src/App.js
+++ web/src/App.js
@@ -1,28 +1,46 @@
 
 // Libs
 import React, { Component } from 'react';
-import { MuiThemeProvider } from 'material-ui/styles';
+import { MuiThemeProvider, withTheme } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 
+
 // App resources
-import theme from './theme';
-import Pattern from './Pattern';
-import './App.css';
+import appTheme from './theme';
+import Patterns from './Pattern';
 
 
 export default class App extends Component {
     render() {
         return (
-            <MuiThemeProvider theme={theme}>
-                <Typography variant="display3">Wall Patterns</Typography>
-
-                <div id="patterns">
-                    <Pattern
-                      name="Simple Crawl"
-                      script="simple_crawl"
-                    />
-                </div>
+            <MuiThemeProvider theme={appTheme}>
+                <ThemeHeader />
+                <Patterns />
             </MuiThemeProvider>
         );
     }
 }
+
+
+class Header extends Component {
+    render () {
+        const { theme } = this.props;
+        const primaryColor = theme.palette.primary.main;
+
+        const styles = {
+            primaryColor: {
+                backgroundColor: primaryColor,
+                padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+                color: theme.palette.common.white,
+            },
+        };
+
+        return (
+            <div style={styles.primaryColor}>
+                <Typography variant="display3">Wall Patterns</Typography>
+            </div>
+        );
+    }
+}
+
+const ThemeHeader = withTheme()(Header);
--- web/src/Pattern.js
+++ web/src/Pattern.js
@@ -1,35 +1,80 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
 import Card from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 
 
-export default class Pattern extends Component {
-    static propTypes = {
-        name: PropTypes.string.isRequired,
-        script: PropTypes.string.isRequired
-    };
-
-    constructor (props) {
-        super(props);
-        this.state = {
-            selected: false
-        };
+const patternStyles = {
+    pattern: {
+        cursor: 'pointer',
+        padding: '0.5rem'
     }
+};
 
-    render () {
-        const { name } = this.props;
-        const { selected } = this.state;
+const Pattern = withStyles(patternStyles)(
+    class Pattern extends Component {
+        static propTypes = {
+            name: PropTypes.string.isRequired,
+            script: PropTypes.string.isRequired
+        };
 
-        return (
-            <div className="pattern">
-                <Card>
+        constructor (props) {
+            super(props);
+            this.state = {
+                selected: false
+            };
+        }
+
+        render () {
+            const { classes, name } = this.props;
+            const { selected } = this.state;
+
+            return (
+                <Card className={classes.pattern} >
                     <Typography variant="headline">
                         {name}
                     </Typography>
                 </Card>
-            </div>
-        );
+            );
+        }
     }
-}
+);
+
+
+const styles = {
+    patterns: {
+        display: 'grid',
+        gridGap: '1rem',
+        gridTemplateColumns: 'repeat(3, 1fr)',
+        marginTop: '3rem',
+        padding: '0 2rem',
+    }
+};
+
+export default withStyles(styles)(
+    class Patterns extends Component {
+        static patternOrder = [
+            [
+                { name: 'Simple Crawl', script: 'simple_crawl' }
+            ]
+        ];
+
+        render () {
+            const { classes } = this.props;
+            return (
+                <div className={classes.patterns}>
+                    {Patterns.patternOrder.map((patternRow, i) =>
+                        patternRow.map((patternProps, j) =>
+                            <Pattern
+                              key={patternProps.name}
+                              {...patternProps}
+                            />
+                        )
+                    )}
+                </div>
+            );
+        }
+    }
+)
