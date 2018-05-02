b9af2e6cdc6da344793a858e9d04d3b48954f131
Milo Toor <mtoor@omicia.com>
2018-05-01 21:01:20 -0700

    Export header to its own file, add host/port inputs

--- web/src/App.js
+++ web/src/App.js
@@ -1,46 +1,21 @@
 
 // Libs
 import React, { Component } from 'react';
-import { MuiThemeProvider, withTheme } from 'material-ui/styles';
-import Typography from 'material-ui/Typography';
-
+import { MuiThemeProvider } from 'material-ui/styles';
 
 // App resources
-import appTheme from './theme';
+import Header from './Header';
 import Patterns from './Pattern';
+import appTheme from './theme';
 
 
 export default class App extends Component {
     render() {
         return (
             <MuiThemeProvider theme={appTheme}>
-                <ThemeHeader />
+                <Header />
                 <Patterns />
             </MuiThemeProvider>
         );
     }
 }
-
-
-class Header extends Component {
-    render () {
-        const { theme } = this.props;
-        const primaryColor = theme.palette.primary.main;
-
-        const styles = {
-            primaryColor: {
-                backgroundColor: primaryColor,
-                padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
-                color: theme.palette.common.white,
-            },
-        };
-
-        return (
-            <div style={styles.primaryColor}>
-                <Typography variant="display3">Wall Patterns</Typography>
-            </div>
-        );
-    }
-}
-
-const ThemeHeader = withTheme()(Header);
---
+++ web/src/Header.jsx
@@ -0,0 +1,67 @@
+
+
+// Libs
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withStyles } from 'material-ui/styles';
+import Input from 'material-ui/Input';
+import Typography from 'material-ui/Typography';
+
+// App resources
+import { UPDATE_HOST, UPDATE_PORT } from './store';
+
+const actionCreators = {
+    updateHost: host => ({ type: UPDATE_HOST, host }),
+    updatePort: port => ({ type: UPDATE_PORT, port })
+};
+
+const styles = theme => ({
+    connection: {
+        fontSize: '12px'
+    },
+    header: {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.common.white,
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`
+    },
+    port: {
+        marginLeft: '1rem'
+    }
+});
+
+const Header = connect(state => state, actionCreators)(
+    class Header extends Component {
+        updateHost = event => this.props.updateHost(event.target.value);
+        updatePort = event => this.props.updatePort(event.target.value);
+
+        render () {
+            const { classes, host, port } = this.props;
+
+            return (
+                <div className={classes.header}>
+                    <Typography variant="display3">Wall Patterns</Typography>
+
+                    <div className={classes.connection}>
+                        <Input
+                          classes={{root: classes.connection}}
+                          label="Host"
+                          onChange={this.updateHost}
+                          value={host}
+                        />
+                        <Input
+                          classes={{root: classes.connection}}
+                          className={classes.port}
+                          label="Port"
+                          onChange={this.updatePort}
+                          value={port}
+                        />
+                    </div>
+                </div>
+            );
+        }
+    }
+);
+
+export default withStyles(styles)(Header);
